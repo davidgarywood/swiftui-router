8bfbb9ce11db1d474d2391a7cef221854bdfce25
David Wood <davidw@roboheadz.com>
2021-05-16 21:33:22 +1200

    Update Services comments

--- swiftui-router/Dependencies/Services.swift
+++ swiftui-router/Dependencies/Services.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
+/// Services defines a holder for our App's dependencies, each Router and ViewModel will have access to Services
 protocol Services {
     var defaultsManager: DefaultsManager { get }
     var loginManager: LoginManager { get }
 }
 
+/// Defines our App's services holder, using the protocol above. This will be the 'real' holder for our dependencies
 class AppServices: Services {
     var loginManager: LoginManager
     var defaultsManager: DefaultsManager
@@ -23,6 +25,8 @@ class AppServices: Services {
     }
 }
 
+/// Defines a mock services holder. This can be used as a starting point for mocks for testing our View Models
+/// This is also used for giving to our ViewModels when creating SwiftUI Previews
 class MockServices: Services {
     var defaultsManager: DefaultsManager = MockDefaultsManager()
     var loginManager: LoginManager = MockLoginManager()
